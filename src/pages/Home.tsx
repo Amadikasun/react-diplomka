13d132e09eb94d7224b3adb5ecba71c3b63e638a
Jana Turoňová <amadikasun@gmail.com>
2022-07-06 15:37:45 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,8 +21,8 @@ const [post, setPost] = useState('');
 
     return (
 		<Layout> 
-        <div className='container  w-100'>
-            <div className='row p-3 border bg-light  ' id='center-frame'>
+        <div className='container  w-100 bg-light'>
+            <div className=' col row  ps-5  bg-light  ' id='center-frame'>
                 <div className='col-lg-8 border indigo'>
                     <div className='justify-content-center  p-5 lh-base'>  
                     <Markdown>
@@ -31,9 +31,9 @@ const [post, setPost] = useState('');
                     </div>
                 </div>
                 
-                <div className='col-lg-4 border '>
+                <div className='col-lg-4 bg-white border '>
                     
-                    <div className=' justify-content-center p-3 border bg-white ' id='center-frame'>
+                    <div className=' justify-content-center p-3  ' id='center-frame'>
 
                         <div className='row justify-content-center align-self-center text-center text-wrap fs-7 p-2' >
                             <img className='card-img-top w-75'  src='https://previews.123rf.com/images/melnikof/melnikof1704/melnikof170400174/76760411-nature-spring-blooming-poppy-concept-close-up-on-massed-display-of-blooming-red-poppies-on-a-sunny-s.jpg' alt="poppies"></img>
